c3074a4eca2b26482ff44c19faef391a0da32be7
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-21 11:43:41 -0500

    installed react-router, import into main and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
 
   return (
     <>
       <div id="container">
-        <h1>Hello React Router!</h1>
         <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{/* routes here */}</div>
+        <div id="main-section">
+          <Routes>
+            
+          </Routes>
+        </div>
       </div>
     </>
   )
