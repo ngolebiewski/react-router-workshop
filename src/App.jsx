d3488894faa9c5789b3188f408c1703e4543cea3
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-21 11:49:05 -0500

    added red and blue Route to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ const App = () => {
         <div id="navbar">{/* navigation here */}</div>
         <div id="main-section">
           <Routes>
-            
+            <Route path='/red' element={<h1>Red</h1>} />
+            <Route path='/blue' element={<h1>Blue</h1>} />
           </Routes>
         </div>
       </div>
