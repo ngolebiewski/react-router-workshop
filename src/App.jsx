8ed57a5ebd961ab0ee255a95e790fa9b75974a6f
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-21 11:36:34 -0500

    added start code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">{/* routes here */}</div>
+      </div>
     </>
   )
 }
