a7d1c7e477ec60bb646bcb3ccc8f08f03a907719
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-21 12:09:25 -0500

    add Home, and Link + Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Link } from "react-router-dom";
 import Blue from "./components/Blue.jsx";
 import Red from "./components/Red.jsx";
+import Home from "./components/Home.jsx";
 
 const App = () => {
 
@@ -8,12 +9,14 @@ const App = () => {
     <>
       <div id="container">
         <div id="navbar">
+         <Link to='/'>home</Link>
           <Link to='/blue'>blue</Link>
           <Link to='/red'>red</Link>
         </div>
 
         <div id="main-section">
           <Routes>
+            <Route path='/' element={<Home />} />
             <Route path='/red' element={<Red />} />
             <Route path='/blue' element={<Blue />} />
           </Routes>
